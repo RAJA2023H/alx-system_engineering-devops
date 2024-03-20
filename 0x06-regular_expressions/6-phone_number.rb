629cb47f8703e25c2e0d4b45e57bb56b178f8fa2
rajae <hemmanyrajae@gmail.com>
2024-03-20 15:38:20 -0700

    tip : using $ signe to make sure that onlu phone numbers with 10 digit should much

--- 0x06-regular_expressions/6-phone_number.rb
+++ 0x06-regular_expressions/6-phone_number.rb
@@ -4,4 +4,4 @@
    Requirements: The regular expression must match School
    accepts one argument and pass it to a regular expression matching method
 =end
-puts ARGV[0].scan(/^\d{10}/).join
+puts ARGV[0].scan(/^\d{10}$/).join

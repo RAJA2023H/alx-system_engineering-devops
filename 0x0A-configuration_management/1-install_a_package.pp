92ddf8a59858410a48ee30c52ec99c9eba6641a2
rajae <hemmanyrajae@gmail.com>
2024-04-23 00:29:41 -0700

    need 'Werkzeug before the flask package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,11 @@
 # install flask from pip3
 
 package { 'flask':
-  ensure	=> '2.1.0',
-  provider	=> 'pip3'
+  ensure    => '2.1.0',
+  provider  => 'pip3',
 }
-
+# install need this before the flask package
 package { 'Werkzeug':
-  ensure	=> '2.1.1',
-  provider	=> 'pip3',
+  ensure    => '2.1.1',
+  provider  => 'pip3',
 }

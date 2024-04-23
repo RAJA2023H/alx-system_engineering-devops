8ec842a93d480a16e237bfa10393b83fd05a4071
rajae <hemmanyrajae@gmail.com>
2024-04-22 23:30:25 -0700

    up

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
-# install flask from pip3.
+# Remove existing Flask and Werkzeug packages, and install Flask version 2.1.0i
 package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
+  provider => 'pip3'
 }

91e9e8ce8f895174f4fbca6948af25ac3974f5d4
rajae <hemmanyrajae@gmail.com>
2024-08-13 19:20:27 -0700

    up

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -6,6 +6,7 @@ the first 10 hot posts listed for a given subreddit
 
 import requests
 
+
 def top_ten(subreddit):
     """
     """
@@ -13,10 +14,9 @@ def top_ten(subreddit):
     url = f"https://www.reddit.com/r/{subreddit}/hot.json"
     response = requests.get(url, allow_redirects=False, headers=headers)
     if response.status_code == 200:
-         data = response.json().get("data")
-         posts = data.get("children", [])
-
-         for i, post in enumerate(posts[:10]):
-             print(f"{i+1}. {post['data']['title']}")
+        data = response.json().get("data")
+        posts = data.get("children", [])
+        for i, post in enumerate(posts[:10]):
+            print(f"{i+1}. {post['data']['title']}")
     else:
         print(None)
